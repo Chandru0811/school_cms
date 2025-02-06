f0b20e553dd3efadc286e609500d20db520a9967
manojecs2020 <manoj@ecscloudinfotech.com>
2025-02-06 18:44:48 +0530

    Employee module added

---
+++ src/pages/admin/Employee/Employee.jsx
@@ -0,0 +1,239 @@
+import { useMemo, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { MaterialReactTable } from "material-react-table";
+import {
+  ThemeProvider,
+  createTheme,
+  Menu,
+  MenuItem,
+  IconButton,
+} from "@mui/material";
+import Delete from "../../../components/common/Delete";
+import PropTypes from "prop-types";
+import { MoreVert as MoreVertIcon } from "@mui/icons-material";
+
+function Employee() {
+  const [menuAnchor, setMenuAnchor] = useState(null);
+  const navigate = useNavigate();
+
+  const data = [
+    {
+      id: 1,
+      center_id: "SUB001",
+      role_id: "SUB001",
+      name: "Sumaiya",
+      email: "mailto:sumaiya@gmail.com",
+      createdBy: "Admin",
+      createdAt: "2024-01-01",
+      updatedAt: "2024-02-01",
+      updatedBy: "Moderator",
+    },
+    {
+      id: 2,
+      center_id: "SUB001",
+      role_id: "SUB001",
+      name: "Sumaiya",
+      email: "mailto:sumaiya@gmail.com",
+      createdBy: "Admin",
+      createdAt: "2024-01-01",
+      updatedAt: "2024-02-01",
+      updatedBy: "Moderator",
+    },
+  ];
+
+  const columns = useMemo(
+    () => [
+      {
+        accessorFn: (row, index) => index + 1,
+        header: "S.NO",
+        enableSorting: true,
+        enableHiding: false,
+        size: 40,
+        cell: ({ cell }) => (
+          <span style={{ textAlign: "center" }}>{cell.getValue()}</span>
+        ),
+      },
+      {
+        accessorKey: "id",
+        header: "",
+        enableHiding: false,
+        enableSorting: false,
+        size: 20,
+        Cell: () => (
+          <IconButton
+            onClick={(e) => {
+              e.stopPropagation();
+              setMenuAnchor(e.currentTarget);
+            }}
+          >
+            <MoreVertIcon />
+          </IconButton>
+        ),
+      },
+      {
+        accessorKey: "center_id",
+        header: "Center Name",
+      },
+      {
+        accessorKey: "role_id",
+        header: "Role Name",
+      },
+      {
+        accessorKey: "name",
+        header: "Employee Name",
+      },
+      {
+        accessorKey: "email",
+        header: "Email",
+      },
+      { accessorKey: "created_by", header: "Created By" },
+      {
+        accessorKey: "created_at",
+        header: "Created At",
+        Cell: ({ cell }) => cell.getValue()?.substring(0, 10),
+      },
+      {
+        accessorKey: "updated_by",
+        header: "Updated By",
+        Cell: ({ cell }) => cell.getValue() || "",
+      },
+      {
+        accessorKey: "updated_at",
+        header: "Updated At",
+        Cell: ({ cell }) => cell.getValue()?.substring(0, 10) || "",
+      },
+    ],
+    []
+  );
+
+  const theme = createTheme({
+    components: {
+      MuiTableCell: {
+        styleOverrides: {
+          head: {
+            color: "#535454 !important",
+            backgroundColor: "#e6edf7 !important",
+            fontWeight: "400 !important",
+            fontSize: "13px !important",
+            textAlign: "center !important",
+          },
+        },
+      },
+      MuiSwitch: {
+        styleOverrides: {
+          root: {
+            "&.Mui-disabled .MuiSwitch-track": {
+              backgroundColor: "#f5e1d0",
+              opacity: 1,
+            },
+            "&.Mui-disabled .MuiSwitch-thumb": {
+              color: "#eb862a",
+            },
+          },
+          track: {
+            backgroundColor: "#e0e0e0",
+          },
+          thumb: {
+            color: "#eb862a",
+          },
+          switchBase: {
+            "&.Mui-checked": {
+              color: "#eb862a",
+            },
+            "&.Mui-checked + .MuiSwitch-track": {
+              backgroundColor: "#eb862a",
+            },
+          },
+        },
+      },
+    },
+  });
+
+  const handleMenuClose = () => setMenuAnchor(null);
+
+  return (
+    <div className="container-fluid mb-4 px-0">
+      <ol
+        className="breadcrumb my-3 d-flex align-items-center"
+        style={{ listStyle: "none", padding: 0, margin: 0 }}
+      >
+        <li>
+          <Link to="/" className="custom-breadcrumb text-sm">
+            Home
+          </Link>
+          <span className="breadcrumb-separator"> &gt; </span>
+        </li>
+        <li className="breadcrumb-item active  text-sm" aria-current="page">
+          &nbsp;Employee
+        </li>
+      </ol>
+      <div className="card">
+        <div className="d-flex justify-content-between align-items-center card_header p-2">
+          <div className="d-flex align-items-center">
+            <div className="d-flex">
+              <div className="dot active"></div>
+            </div>
+            <span className="me-2 text-muted  text-sm">
+              This database shows the list of&nbsp;
+              <span className="database_name">Employee</span>
+            </span>
+          </div>
+          <Link to="/employee/Add">
+            <button
+              type="button"
+              className="btn btn-button btn-sm me-2"
+              style={{ fontWeight: "600px !important" }}
+            >
+              &nbsp; Add &nbsp;&nbsp; <i className="bi bi-plus-lg"></i>
+            </button>
+          </Link>
+        </div>
+        <>
+          <ThemeProvider theme={theme}>
+            <MaterialReactTable
+              columns={columns}
+              data={data}
+              enableColumnActions={false}
+              enableColumnFilters={false}
+              enableDensityToggle={false}
+              enableFullScreenToggle={false}
+              initialState={{
+                columnVisibility: {
+                  working_hrs: false,
+                  citizenship: false,
+                  nationality: false,
+                  created_by: false,
+                  created_at: false,
+                  updated_by: false,
+                  updated_at: false,
+                },
+              }}
+              muiTableBodyRowProps={() => ({
+                onClick: () => navigate(`/employee/View`),
+                style: { cursor: "pointer" },
+              })}
+            />
+          </ThemeProvider>
+          <Menu
+            id="action-menu"
+            anchorEl={menuAnchor}
+            open={Boolean(menuAnchor)}
+            onClose={handleMenuClose}
+          >
+            <MenuItem onClick={() => navigate(`/employee/Edit`)}>Edit</MenuItem>
+            <MenuItem>
+              <Delete path={`admin/company/delete`} onOpen={handleMenuClose} />
+            </MenuItem>
+          </Menu>
+        </>
+      </div>
+    </div>
+  );
+}
+
+Employee.propTypes = {
+  row: PropTypes.func.isRequired,
+  cell: PropTypes.func.isRequired,
+};
+
+export default Employee;
---
+++ src/pages/admin/Employee/EmployeeView.jsx
@@ -0,0 +1,123 @@
+import { Link } from "react-router-dom";
+
+function EmployeeView() {
+  const data = {
+    center_id: "School A",
+    role_id: "Junior Developer",
+    name: "Sumaiya",
+    email: "sumaiya@gmail.com",
+    password: "12345678",
+    mobile: "9043878105",
+  };
+
+  return (
+    <div className="container-fluid px-0">
+      <ol
+        className="breadcrumb my-2 px-2"
+        style={{ listStyle: "none", padding: 0, margin: 0 }}
+      >
+        <li>
+          <Link to="/" className="custom-breadcrumb text-sm">
+            Home
+          </Link>
+          <span className="breadcrumb-separator"> &gt; </span>
+        </li>
+        <li>
+          <Link to="/employee" className="custom-breadcrumb text-sm">
+            &nbsp;Employee
+          </Link>
+          <span className="breadcrumb-separator"> &gt; </span>
+        </li>
+        <li className="breadcrumb-item active text-sm" aria-current="page">
+          &nbsp;Employee View
+        </li>
+      </ol>
+      <div className="card vh-100" style={{ border: "1px solid #dbd9d0" }}>
+        <div className="d-flex px-4 justify-content-between align-items-center card_header p-1 mb-4">
+          <div className="d-flex align-items-center">
+            <div className="d-flex">
+              <div className="dot active"></div>
+            </div>
+            <span className="me-2 text-muted">View Employee</span>
+          </div>
+          <div className="my-2 pe-3 d-flex align-items-center">
+            <Link to="/employee">
+              <button type="button " className="btn btn-sm btn-back">
+                Back
+              </button>
+            </Link>
+            &nbsp;&nbsp;
+          </div>
+        </div>
+        <div className="container-fluid px-4">
+          <div className="row pb-3">
+            <div className="col-md-6 col-12 my-2">
+              <div className="row">
+                <div className="col-6">
+                  <p className="fw-medium text-sm">Centre Name</p>
+                </div>
+                <div className="col-6">
+                  <p className="text-muted text-sm">: {data.center_id}</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6 col-12 my-2">
+              <div className="row">
+                <div className="col-6">
+                  <p className="fw-medium text-sm">Role Name</p>
+                </div>
+                <div className="col-6">
+                  <p className="text-muted text-sm">: {data.role_id}</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6 col-12 my-2">
+              <div className="row">
+                <div className="col-6">
+                  <p className="fw-medium text-sm">Employee Name</p>
+                </div>
+                <div className="col-6">
+                  <p className="text-muted text-sm">: {data.name}</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6 col-12 my-2">
+              <div className="row">
+                <div className="col-6">
+                  <p className="fw-medium text-sm">Employee Email</p>
+                </div>
+                <div className="col-6">
+                  <p className="text-muted text-sm">: {data.email}</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6 col-12 my-2">
+              <div className="row">
+                <div className="col-6">
+                  <p className="fw-medium text-sm">Employee Email</p>
+                </div>
+                <div className="col-6">
+                  <p className="text-muted text-sm">: {data.email}</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6 col-12 my-2">
+              <div className="row">
+                <div className="col-6">
+                  <p className="fw-medium text-sm">Employee Password</p>
+                </div>
+                <div className="col-6">
+                  <p className="text-muted text-sm text-break ">
+                    : {data.password}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default EmployeeView;
