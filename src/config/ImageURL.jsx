420c5069cc5a504d0af85e07e090316689f59a51
Chandru0811 <chandru08112000@gmail.com>
2025-02-23 17:53:26 +0530

    url changed

--- src/config/ImageURL.jsx
+++ src/config/ImageURL.jsx
@@ -1,4 +1,4 @@
-// const ImageURL = "https://sgitjobs.com/schoolCms/";
+// const ImageURL = "https://sgitjobs.com/school-cms/";
 const ImageURL = "http://127.0.0.1:8000/";
 
 export default ImageURL;
--- src/config/URL.jsx
+++ src/config/URL.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://sgitjobs.com/schoolCms/api/",
+  // baseURL: "https://sgitjobs.com/school-cms/api/",
   baseURL: "http://127.0.0.1:8000/api/",
 });
 
